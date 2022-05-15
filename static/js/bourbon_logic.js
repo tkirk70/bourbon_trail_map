5cd9ac0f42f2d8f8030b901c55974a5acf62aed1
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-05-14 21:31:46 -0400

    Adding tileSize and zoomOffset params to .js file.

--- static/js/bourbon_logic.js
+++ static/js/bourbon_logic.js
@@ -6,6 +6,8 @@ console.log("working");
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
+  tileSize: 512,
+  zoomOffset: -1
   accessToken: API_KEY
 });
 
